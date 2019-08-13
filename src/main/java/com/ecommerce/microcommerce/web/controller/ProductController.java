a327f1ec3b2050b1a2ce959879cef64eb422f3e4
hostaur <aureliehostens@gmail.com>
2019-08-13 13:52:24 -0400

    premiere partie: calculerMargeProduit

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -1,22 +1,35 @@
 package com.ecommerce.microcommerce.web.controller;
 
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.MappingJacksonValue;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.validation.Valid;
-import java.net.URI;
-import java.util.List;
 
 
 @Api( description="API pour es opérations CRUD sur les produits.")
@@ -27,7 +40,26 @@ public class ProductController {
     @Autowired
     private ProductDao productDao;
 
+    @ApiOperation(value = "Récupère la liste des produits avec leur marge")
+	@GetMapping(value = "/AdminProduits")
+	public Map<Object, Integer> calculerMargeProduit() {
 
+		Iterable<Product> produits = productDao.findAll();
+
+		SimpleBeanPropertyFilter monFiltre = SimpleBeanPropertyFilter.serializeAllExcept("prixAchat");
+
+		FilterProvider listDeNosFiltres = new SimpleFilterProvider().addFilter("monFiltreDynamique", monFiltre);
+		Map<Object, Integer> map = new HashMap<>();
+		produits.forEach(produit -> {
+
+			MappingJacksonValue produitFiltres = new MappingJacksonValue(produit);
+			produitFiltres.setFilters(listDeNosFiltres);
+			map.put(produitFiltres.getValue(), produit.getPrix() - produit.getPrixAchat());
+
+		});
+
+		return map;
+	}
     //Récupérer la liste des produits
 
     @RequestMapping(value = "/Produits", method = RequestMethod.GET)
